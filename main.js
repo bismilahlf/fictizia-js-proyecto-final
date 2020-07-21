7318265cd8a7ceade4420fda461752376fd34d52
Bismilah Langford <bismilah.lf@gmail.com>
2020-07-21 04:35:43 +0200

    Modify styles & refactor js

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const myHouse = document.querySelector('.my-house');
 const sortingBtn = document.querySelector('.sorting-btn');
+const mainContainer = document.querySelector('.container');
 
 const request = 'https://www.potterapi.com/v1/sortinghat';
-
-sortingBtn.addEventListener('click', handleSortingButton);
-
-function getSorted(json) {
-    myHouse.innerText = json;
-}
 
 function handleSortingButton() {
     fetch(request)
     .then(res => res.json())
-    .then(json => getSorted(json))
+    .then(data => {
+        myHouse.innerText = data;
+        if(myHouse.innerText === 'Gryffindor') {
+            mainContainer.classList.add('gryffindor');
+        } else {
+            mainContainer.classList.remove('gryffindor');
+        }
+    })
 }
+sortingBtn.addEventListener('click', handleSortingButton);
+
+
 
 
