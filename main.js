dc83ef636aa81e238b74b9f0ac6b2aaa26567d02
Bismilah Langford <bismilah.lf@gmail.com>
2020-07-22 18:35:34 +0200

    Add change color functionality

--- main.js
+++ main.js
@@ -9,11 +9,37 @@ function handleSortingButton() {
     .then(res => res.json())
     .then(data => {
         myHouse.innerText = data;
-        if(myHouse.innerText === 'Gryffindor') {
-            mainContainer.classList.add('gryffindor');
-        } else {
-            mainContainer.classList.remove('gryffindor');
+        console.log(data)
+        switch(data) {
+            case 'Gryffindor':
+                mainContainer.classList.add('gryffindor');
+                mainContainer.classList.remove('slytherin');
+                mainContainer.classList.remove('hufflepuff');
+                mainContainer.classList.remove('ravenclaw');
+                break;
+            case 'Slytherin':
+                mainContainer.classList.add('slytherin');
+                mainContainer.classList.remove('gryffindor');
+                mainContainer.classList.remove('hufflepuff');
+                mainContainer.classList.remove('ravenclaw');
+                break;
+            case 'Hufflepuff':
+                mainContainer.classList.add('hufflepuff');
+                mainContainer.classList.remove('slytherin');
+                mainContainer.classList.remove('gryffindor');
+                mainContainer.classList.remove('ravenclaw');
+                break;
+            case 'Ravenclaw':
+                mainContainer.classList.add('ravenclaw');
+                mainContainer.classList.remove('slytherin');
+                mainContainer.classList.remove('hufflepuff');
+                mainContainer.classList.remove('gryffindor');
+                break;
+            default:
+                mainContainer.classList.add('default');
         }
+            
+        
     })
 }
 sortingBtn.addEventListener('click', handleSortingButton);
